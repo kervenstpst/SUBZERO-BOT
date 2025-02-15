dceadcb719457a9701bcf86e53a1aebb386100e8
kervenstpst <botipalone@gmail.com>
2025-02-15 15:22:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~KUQhlKiJ#tzqHlp56pOgAFG0Wc6nj7kDsmauFfc7opHpHOHtjU1c",
+SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~mUwmWAAa#LE49zpGxgGff3zgPjv7H8GOr7-uk10wvv1sBtm-kKu0",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -23,9 +23,9 @@ CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
+DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50941220398",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50947805453",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "᭄͢🌹𝗠𝗥᭄⃟‌𝗔𝗥𝗟𝗢𝗥𝗡𝗘☂𐏓ᬁ𝗠𝗣𝗞⍣᭄͢🌹",
 // add bot owner name

cbc5be2800a6e0337c6e793f902bdd2b2239eb97
kervenstpst <botipalone@gmail.com>
2025-02-16 07:29:17 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~mUwmWAAa#LE49zpGxgGff3zgPjv7H8GOr7-uk10wvv1sBtm-kKu0",
+SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~GIZ20bRI#XQzgRjY_6SUbeoiXUtDyqunzIZj0rj6-5KapSOCvSx8",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -25,7 +25,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50947805453",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "50941220398",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "᭄͢🌹𝗠𝗥᭄⃟‌𝗔𝗥𝗟𝗢𝗥𝗡𝗘☂𐏓ᬁ𝗠𝗣𝗞⍣᭄͢🌹",
 // add bot owner name
@@ -41,7 +41,7 @@ ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
@@ -55,7 +55,7 @@ PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
-DEV: process.env.DEV || "50947805453",
+DEV: process.env.DEV || "",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
